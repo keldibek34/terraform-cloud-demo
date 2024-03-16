6328723991b6c52eab85104f6cb7f3548f00a827
keldibek34 <153963679+keldibek34@users.noreply.github.com>
2024-03-16 15:40:10 -0500

    added asg

---
+++ asg/data.tf
@@ -0,0 +1,43 @@
+# Pulls VPC related info
+
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "kabysov"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+
+# DB related info
+
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "kabysov"
+    workspaces = {
+      name = "rds"
+    }
+  }
+}
+
+
+# AMI info
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
